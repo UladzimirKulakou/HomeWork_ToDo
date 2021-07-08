c1fd648b749f31212794623d80a49dade30c21cd
Uladzimir Kulakou <rombik9@ya.ru>
2021-07-08 20:37:11 +0300

    commit 1
    create project

---
+++ ToDo_App/ToDo_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo_App
+//
+//  Created by Uladzimir Kulakou on 7/8/21.
+//  Copyright © 2021 Uladzimir Kulakou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
